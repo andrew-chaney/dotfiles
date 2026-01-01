4aced935fcff682b577a2f9d96163284f1a9d690
Andrew Chaney <andrew.chaney24@proton.me>
2026-01-01 14:18:43 -0700

    more colorschemes and updated default

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "erikbackman/brightburn.vim" },
+  { "folke/tokyonight.nvim" },
+  { "rose-pine/neovim" },
+  { "catppuccin/nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "xero/miasma.nvim" },
+  { "NLKNguyen/papercolor-theme" },
+  { "neanias/everforest-nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ron",
+    },
+  },
+}
