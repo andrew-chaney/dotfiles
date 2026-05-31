0b418e572794bdf60c2648e9631f8787334fb748
Andrew Chaney <andrew.chaney24@proton.me>
2026-05-30 21:33:05 -0600

    fixing blink shell nvim esc binding

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,20 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Fix for Blink Shell + iPadOS adding a newline on Cmd-.
-vim.keymap.set("i", "<D-.>", function()
-  -- Drop to normal mode
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
-
-  -- Clear the next keypress if it happens to be the accidental Enter key
-  vim.defer_fn(function()
-    local char = vim.fn.getchar(0)
-    -- 13 is the ASCII code for Carriage Return (Enter)
-    if char == 13 then
-      -- Do nothing, effectively swallowing the phantom newline
-    elseif char > 0 then
-      -- If it was a legitimate keypress instead, feed it back to Neovim
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(vim.fn.nr2char(char), true, false, true), "m", false)
-    end
-  end, 10) -- 10ms delay is enough to capture the trailing iPadOS sequence
-end, { desc = "Fix Blink Shell Cmd-. newline bug" })
+-- Blink Shell on iPadOS: Cmd-. is configured as an Esc substitute
+-- (Magic Keyboard lacks a physical Esc key).
+--
+-- Blink may send either:
+--   <D-.>  - Kitty keyboard protocol event (Cmd+period as a unit)
+--   <M-.>  - \e. byte sequence (ESC + period), where the bare \e would exit
+--            insert mode and the trailing . would fire dot-repeat in normal
+--            mode, re-inserting the last change as a phantom "extra character"
+--
+-- Mapping both to <Esc> is safe universally: a real Esc key sends a single
+-- \x1b byte (unaffected), and neither combo has a standard insert-mode binding
+-- that other clients depend on.
+vim.keymap.set("i", "<D-.>", "<Esc>", { noremap = true })
+vim.keymap.set("i", "<M-.>", "<Esc>", { noremap = true })
